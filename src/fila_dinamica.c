bd1aa99461846c54746cb011cb58245db78128dc
Nerton <thiago2nerton@gmail.com>
2024-02-12 01:25:15 -0300

    Viver

--- src/fila_dinamica.c
+++ src/fila_dinamica.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fila_dinamica.h"
+
+Fila * criar();
+int inserir(Fila *, struct carta);
+int remover(Fila *, struct carta *); // Já remove e acessa
+int acessar(Fila *, struct carta *);
+int exibir(Fila *);
+int tamanho(Fila *)i;
