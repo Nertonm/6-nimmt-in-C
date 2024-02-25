eef3b004c539ce199a5b4785f6d8d4de863483b1
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-02-25 19:38:38 -0300

    Update lista_dinamica.h

--- src/lista_dinamica.h
+++ src/lista_dinamica.h
@@ -1,1 +1,18 @@
 #include "cartaStruct.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct elementos *Lista;
+
+Lista* criar();
+
+int inserirOrdenado(Lista*, struct carta);
+
+int acessarIndice(Lista*, int,struct carta*);
+
+int removerIndince(Lista*,int);
+
+int exibir(Lista*);
+
+int quantidade(Lista*);
