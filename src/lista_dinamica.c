d554b6af1383e26c4bec1cd5522cca25acb382c2
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-02-25 19:36:54 -0300

    Update lista_dinamica.c

--- src/lista_dinamica.c
+++ src/lista_dinamica.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include "lista_dinamica.h"
+#include "cartaStruct.h"
+#include <stdio.h>
+struct elemento{
+    struct carta dados;
+    struct elemento *proximo;
+    };
+typedef struct elemento Elemento;
+
+Lista* criar(){
+    Lista *mao; //baralho das cartas//
+    mao=(Lista*)malloc(sizeof(Lista));
+    if (mao!=NULL){
+        *mao=NULL;
+    }
+    return mao;
+}
+
+int inserirOrdenado(Lista mao*, struct carta novosdados){
+    if (mao==NULL)return 0;
+    Elemento *novo=(Elemento*)malloc(sizeof(Elemento));
+    if (novo==NULL)return 0;
+    novo->dados = novosdados;
+    if (*mao==NULL || (*mao)->dados.
+    }
 
