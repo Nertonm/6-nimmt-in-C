91d35bc8fcc71e404b5f530fcdaffa158d1c21dd
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-02-26 19:57:30 -0300

    Update pilha_dinamica.h

--- src/pilha_dinamica.h
+++ src/pilha_dinamica.h
@@ -1,2 +1,18 @@
 #include "cartaStruct.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct elemento* Pilha;
+
+Pilha* criarp();
+
+int inserirp(Pilha*, struct card);
+
+int removerp(Pilha*, struct card);
+
+void embaralharp(Pilha*);
+
+int tamamhop (Pilha*);
+
 
