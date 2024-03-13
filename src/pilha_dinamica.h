533e9d8370757498a83e09d6d9ddec9b15888991
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-03-13 11:52:29 -0300

    Update pilha_dinamica.h

--- src/pilha_dinamica.h
+++ src/pilha_dinamica.h
@@ -9,7 +9,7 @@ Pilha pilhaCriar();
 
 int pilhaInserir(Pilha *, struct card);
 
-int pilhaRemover(Pilha *, struct card);
+int pilhaRemover(Pilha *, struct card *);
 
 void pilhaEmbaralhar(Pilha *);
 
