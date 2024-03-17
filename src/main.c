efb9662fb4b1ed840421430717b2fd1714782808
Nerton <thiago2nerton@gmail.com>
2024-03-17 00:30:55 -0300

    今本当におやすみなさい

--- src/main.c
+++ src/main.c
@@ -6,26 +6,27 @@ Estruturas necessárias no main:
 - Baralho (Pilha *), inicia com 104 cartas embaralhadas, depois distribui 4 para mesa e 10 para cada jogador
 - Coleção dos Jogadores (Lista **), vetor de Listas contendo as cartas adquiridas por cada jogador, use alocação dinâmica
 */
-
+// Estilo da Arte ASCII e de 40 carcters//
 int main()
 {
-    printf(" #####                                 \n");
-    printf("#     #    #    # # #    # #    # #####\n");
-    printf("#          ##   # # ##  ## ##  ##   #  \n");
-    printf("######     # #  # # # ## # # ## #   #  \n");
-    printf("#     #    #  # # # #    # #    #   #  \n");
-    printf("#     #    #   ## # #    # #    #   #  \n");
-    printf(" #####     #    # # #    # #    #   #  \n");
+    // Estilo da Arte ASCII e de 40 carcters//
+    printf(" #####                                  \n");
+    printf("#     #    #    # # #    # #    # ##### \n");
+    printf("#          ##   # # ##  ## ##  ##   #   \n");
+    printf("######     # #  # # # ## # # ## #   #   \n");
+    printf("#     #    #  # # # #    # #    #   #   \n");
+    printf("#     #    #   ## # #    # #    #   #   \n");
+    printf(" #####     #    # # #    # #    #   #   \n");
     // Inicialização do Jogo
         // Declaração de Variaveis
-            int numBots = 2; /* Iniciado com Dois para situar o caso base do Select Players */
+            int numBots = 2; /* Iniciado em Dois para situar o caso base do Select Players */
         // Selecionando o Numero de Jogadores
             do {
-                printf("====================================\n");
+                printf("========================================\n");
                 if (numBots < 2 || numBots > 9)
-                    printf("|         Selecione Novamente      |\n");
-                printf("|         Quantos Jogadores?       |\n");
-                printf("====================================\n");
+                    printf("|           Selecione Novamente        |\n");
+                printf("|           Quantos Jogadores?         |\n");
+                printf("========================================\n");
                 scanf("%i",&numBots);
             } while (numBots < 2 || numBots > 9);
         // Iniciado o Baralho dos Jogadores |TO DO|
