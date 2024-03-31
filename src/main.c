8b45c3b5632e4b9698558608f57b1efeffc611bd
Nerton <thiago2nerton@gmail.com>
2024-03-30 23:12:30 -0300

    Change main looks

--- src/main.c
+++ src/main.c
@@ -2,35 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-Estruturas necessárias no main:
-- Mão (Lista *), começa com 10 cartas diminui a cada rodada
-- Mesa (Fila **), é um vetor de Filas, use alocação dinâmica
-- Baralho (Pilha *), inicia com 104 cartas embaralhadas, depois distribui 4 para mesa e 10 para cada jogador
-- Coleção dos Jogadores (Lista **), vetor de Listas contendo as cartas adquiridas por cada jogador, use alocação dinâmica
-*/
 int main()
 {
     // Inicio do Jogo
-        // Declaração de Variaveis
-            int numPlayers = msgIni();    /* Iniciado em Dois para situar o caso base do Select Players */
+            //Selecionando o numero de Jogadores
+            int numPlayers = msgIni();               
             int numMesa = 4;              /* Iniciando o valor da mesa para o padrão do jogo */
-        // Iniciando as Estrutura precisas;
+        // Iniciando as Estruturas
             Pilha* monteCartas = pilhaCriar();
             pilhaEmbaralhar(monteCartas);
             Lista* baralhoJogadores[numPlayers];
             Lista* pontosJogadores[numPlayers];
             Fila *mesa[numMesa];
-
             iniGame(baralhoJogadores, monteCartas, numPlayers, pontosJogadores);
         // Iniciando a mesa
             iniMesa(mesa,monteCartas,numMesa);
         // Loop do Jogo
             loopGame(baralhoJogadores,monteCartas,mesa,numMesa, numPlayers,pontosJogadores);
-        // Contagem de Pontos   |TO DO|
+        // Fim do Jogo    
             endGame(pontosJogadores, numPlayers);
-        // Checagem de Vitoria  |TO DO|
-    // Fim de Jogo              |TO DO|
-        // Dizer Pontuação      |TO DO|
     return 0;
 }
